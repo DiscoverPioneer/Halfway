6fa6e8a008de1619090fd8f51247903ac2c6fced
Phil <dplaxer25@aol.com>
2014-02-07 21:30:59 -0500

    Initial Commit

---
+++ Halfway/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Halfway
+//
+//  Created by Phil Scarfi on 2/7/14.
+//  Copyright (c) 2014 Pioneer Mobile Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
